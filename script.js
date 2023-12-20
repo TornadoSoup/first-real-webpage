c722027c0f26d12a6632a7495104c312335dbbdd
TornadoSoup <148398451+TornadoSoup@users.noreply.github.com>
2023-12-20 14:28:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var hiddenText = document.getElementById("hiddenText");
-  hiddenText.style.display = "block"; // Change the display property to make the text visible
-});
+// Konami Code
+const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'KeyB', 'KeyA'];
+let konamiCodeIndex = 0;
+
+function konamiCodeHandler(event) {
+  const key = event.code;
+
+  if (key === konamiCode[konamiCodeIndex]) {
+    konamiCodeIndex++;
+
+    if (konamiCodeIndex === konamiCode.length) {
+      // Konami Code entered successfully, display the game (you can customize this part)
+      alert('Cheat code activated! Game unlocked!');
+      konamiCodeIndex = 0; // Reset the code index for potential future use
+    }
+  } else {
+    konamiCodeIndex = 0; // Reset the code index if the entered key is incorrect
+  }
+}
+
+document.addEventListener('keydown', konamiCodeHandler);
