f0d376c51693c886464d63788e9d2c2abf2a75d6
TornadoSoup <148398451+TornadoSoup@users.noreply.github.com>
2023-12-20 12:08:40 -0600

    Create script.js
    created file and made fade-in logic

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var hiddenText = document.getElementById("hiddenText");
+  hiddenText.style.display = "block"; // Change the display property to make the text visible
+});
